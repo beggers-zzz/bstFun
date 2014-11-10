e901ec0b6edfcb9474efff90055da545f1e54e53
Ben Eggers <beggerss@cs.washington.edu>
2014-11-10 14:52:14 -0800

    wrote height

--- Treap.java
+++ Treap.java
@@ -1,13 +1,35 @@
 /* Ben Eggers
  *
  * This class is a treap--a type of randomized binary search tree. For more
- * information, please see http://en.wikipedia.org/wiki/Treap.
+ * information, see http://en.wikipedia.org/wiki/Treap.
  */
 
+import java.util.Random;
+
 public class Treap<T implements Comparable<T>> {
+    private TreapNode root;
 
+    public Treap<T>() {
 
+    }
 
+    /*
+     * Returns the height of the treap. This is defined to be the maximum
+     * number of hops necessary to get from the root to a leaf node.
+     */
+    public int height() {
+        return height(root);
+    }
+
+    // Horribly inefficient, but oh well! That's what you get for writing
+    // pedegogic code
+    private int height(TreapNode r) {
+        if (r == null || (r.right == null && r.left == null)) {
+            return 0;
+        } else {
+            return 1 + max(height(r.right), height(r.left));
+        }
+    }
 
     /*
      * Node class for the Treap. Pretty much a basic binary search tree node.
