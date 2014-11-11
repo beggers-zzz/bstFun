2721ccca5c89eaafd24a681a45e9b72592b289da
Ben Eggers <beggerss@cs.washington.edu>
2014-11-10 16:26:56 -0800

    binary tree done

---
+++ BinaryTree.java
@@ -0,0 +1,47 @@
+/* Ben Eggers
+ *
+ * This class is an actual binary tree. Nothing fancy.
+ */
+
+public class BinaryTree<T extends Comparable<T>> extends AbstractBinaryTree<T> {
+
+    public void remove(T data) {
+        root = remove(data, root);
+    }
+
+    // Recursive helper for remove()
+    private TreeNode remove(T data, TreeNode r) {
+        if (r == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (r.data.equals(data)) {
+            return null;
+        } else if (r.data.compareTo(data) > 0) {
+            r.left = remove(data, r.left);
+        } else if (r.data.compareTo(data) < 0) {
+            r.right = remove(data, r.right);
+        }
+
+        // Should never happen
+        throw new RuntimeException();
+    }
+
+    public void insert(T data) {
+        root = insert(data, root);
+    }
+
+    // Helper for insert()
+    private TreeNode insert(T data, TreeNode r) {
+        if (r == null) {
+            return new TreeNode(data);
+        } else if (r.data.compareTo(data) > 0) {
+            r.left = insert(data, r.left);
+        } else if (r.data.compareTo(data) < 0) {
+            r.right = insert(data, r.right);
+        }
+
+    // We should never get here
+    throw new RuntimeException();
+    }
+}
