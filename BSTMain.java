86efd53ad25628992c9463df7de9ceae61bd6a7e
Ben Eggers <beggerss@cs.washington.edu>
2014-11-17 14:54:04 -0800

    screw testing. added main

---
+++ BSTMain.java
@@ -0,0 +1,12 @@
+/* Ben Eggers
+ *
+ * This class' main method creates a BST, RandomizedBST, and Treap, and does
+ * some inserts/removals on them to test their effectiveness.
+ */
+
+public class BSTMain {
+
+    public static void main(String[] args) {
+
+    }
+}
