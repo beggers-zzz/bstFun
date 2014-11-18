06d4d8c2049a8de0f678a92c75dd5ebae1701332
Ben Eggers <beggerss@cs.washington.edu>
2014-11-17 21:45:41 -0800

    basic program to make sure BST works

--- BSTMain.java
+++ BSTMain.java
@@ -7,6 +7,11 @@
 public class BSTMain {
 
     public static void main(String[] args) {
-
+        BST<Integer> t = new BST<Integer>();
+        for (int i = 0; i < 100; i++) {
+            t.insert(i);
+        }
+        int h = t.height();
+        System.out.println("Height for linked list tree is " + h);
     }
 }
