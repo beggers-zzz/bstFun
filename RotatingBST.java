76d8e7ac49628050475f0337ebdff512f101064a
Ben Eggers <beggerss@cs.washington.edu>
2014-11-29 17:40:12 -0800

    other rotations

--- RotatingBST.java
+++ RotatingBST.java
@@ -4,5 +4,19 @@
  */
 
 public abstract class RotatingBST<T> extends AbstractBST {
+    public TreeNode rotRightRight(TreeNode root) {
+        return null;
+    }
 
+    public TreeNode rotRightLeft(TreeNode root) {
+        return null;
+    }
+
+    public TreeNode rotLeftLeft(TreeNode root) {
+        return null;
+    }
+
+    public TreeNode rotLeftRight(TreeNode root) {
+        return null;
+    }
 }
